0c450cea120d6f3ba00face94b064d29d7677631
seonjae790 <bbodaewall@gmail.com>
2025-09-04 07:32:03 +0000

    [ADD] TestCase - PostTest.java

--- src/test/java/com/jadecross/guestbook/PostTest.java
+++ src/test/java/com/jadecross/guestbook/PostTest.java
@@ -1,1 +1,14 @@
-packagecom.jadecross.guestbook;import staticorg.junit.jupiter.api.Assertions.assertEquals;importorg.junit.jupiter.api.Test;classPostTest {@TestvoidtestSetName() {Post post= newPost("TESTER",  "2022-04-12 01:01:48",  "Happy Wedding");post.setName("DEVOPS");assertEquals("DEVOPS",  post.getName());}}
+package com.jadecross.guestbook;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class PostTest {
+
+    @Test
+    void testSetName() {
+        Post post = new Post("TESTER", "2022-04-12 01:01:48", "Happy Wedding");
+        post.setName("DEVOPS");
+        assertEquals("DEVOPS", post.getName());
+    }
+}
